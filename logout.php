36dd1600a9b44ddca4faf96808f5e72131ff3a1b
jogemu <40940803+jogemu@users.noreply.github.com>
2025-02-19 15:48:15 +0000

    Comments for logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
-<?php header('Location:'.(parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) == $_SERVER['HTTP_HOST'] ? $_SERVER['HTTP_REFERER'] : '/'));
-if(isset($_COOKIE[$session_name = session_name()])) {
+<?php
+
+// Redirect to referer if same host.
+header('Location:'.(parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) == $_SERVER['HTTP_HOST'] ? $_SERVER['HTTP_REFERER'] : '/'));
+
+// Only destroy session if session cookie is set.
+if(isset($_COOKIE[session_name()])) {
   session_start();
   session_destroy();
-  setcookie($session_name, "", 1, '/');
+  setcookie(session_name(), "", 1, '/'); // Delete cookie
 }
